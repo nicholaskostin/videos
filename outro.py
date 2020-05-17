7b90dbb8e1bffff9150a5c168480668f1547e1db
Nicholas Kostin <kostinnicholas@gmail.com>
2020-05-17 14:03:37 -0600

    fixed outro

--- outro.py
+++ outro.py
@@ -6,44 +6,60 @@ from manimlib.imports import *
 class Outro(Scene):
 
     def construct(self):
-    
-        github_logo = SVGMobject("github")
-        github_logo.set_color(GOLD_A)
+       
+        github_logo = SVGMobject('github').scale(0.6)
+        github_logo.set_color(WHITE)
+        github_logo.move_to(1.6*LEFT)
         
-        github = TextMobject("nicholaskostin")
-        github.set_color(GOLD_A)
-        github.next_to(github_logo.get_center(), 1.6*RIGHT)
+        github = TexMobject('\\texttt{github.com/nicholaskostin}').scale(0.6)
+        github.next_to(github_logo, RIGHT)
+        github_3b1b = TexMobject('\\texttt{github.com/3b1b/manim}').scale(0.6)
+        github_3b1b.next_to(github_logo, RIGHT)
         
-        github_3b1b = TextMobject("manim")
-        github_3b1b.set_color(GOLD_A)
-        github_3b1b.next_to(github_logo.get_center(), 1.6*RIGHT)
+        see_description = TextMobject('See description for source code').scale(1.2)
+        see_description.set_color_by_gradient(RED, BLUE, RED)
         
-        outro_text = TextMobject("See description for source code!")
-        outro_text.move_to(2*UP)
-        
-        credit_text = TextMobject("Creating using 3Blue1Brown's Manim library")
-        credit_text.move_to(2*UP)
+        acknowledge_3b1b = TextMobject(
+            'Created using Manim ',
+            '(by ',
+            '3Blue',
+            '1Brown',
+            ')'
+        ).scale(1.2)
+        acknowledge_3b1b[0].set_color_by_gradient(YELLOW, GREEN)
+        acknowledge_3b1b[1].set_color(WHITE)
+        acknowledge_3b1b[2].set_color(BLUE)
+        acknowledge_3b1b[3].set_color(GREY_BROWN)
+        acknowledge_3b1b[4].set_color(WHITE)
+        acknowledge_3b1b.move_to(1.2*UP)
         
         self.play(
-            Write(outro_text),
+            Write(see_description),
+            run_time = 2
+        )
+        
+        self.play(
+            ApplyMethod(see_description.shift, 1.2*UP)
+        )
+        
+        self.play(
             Write(github_logo),
             Write(github),
+            run_time = 2
+        )
+        
+        self.wait()
+        
+        self.play(
+            Transform(github, github_3b1b),
+            Transform(see_description, acknowledge_3b1b),
             run_time = 2
         )
         
         self.wait(2)
         
         self.play(
-            Transform(outro_text, credit_text),
-            Transform(github, github_3b1b),
-            run_time = 1
-        )
-        
-        self.wait(2)
-        
-        self.play(
-            FadeOut(credit_text),
             FadeOut(github_3b1b),
-            FadeOut(github_logo),
-            run_time = 1.6
+            FadeOut(acknowledge_3b1b),
+            run_time = 2
         )
